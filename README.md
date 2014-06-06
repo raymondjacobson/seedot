98aadfb66a2a4ebcc516bf5844a251048a4fd0dc
Raymond Jacobson <raymondshujacobson8@gmail.com>
2014-06-05 22:42:30 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+#seedot
+
+######programmatically visualize random data with a seed
+------
+
+###running
+```shell
+$ git clone github.com/raymondjacobson/seedot
+$ cd seedot
+$ npm install stylus -g
+$ stylus public/style
+$ go run server.go  # or gin run server.go
+```
+
+###tech
+- Martini
+- D3js
+
+###MIT License... or something
--- server.go
+++ server.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+  "github.com/go-martini/martini"
+  "github.com/codegangsta/martini-contrib/render"
+)
 
 func main() {
   m := martini.Classic()
+  m.Use(render.Renderer(render.Options{
+    Directory: "template",
+    Layout: "layout",
+    Extensions: []string{".html"},
+  }))
 
   m.Get("/", func() string {
     return "Welcome to seedot"
