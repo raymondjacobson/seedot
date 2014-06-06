c761bce914d95c38a25bc7ca11fae43c316262da
Raymond Jacobson <raymondshujacobson8@gmail.com>
2014-06-06 00:04:32 -0700

    build basic randomizer and styling

--- server.go
+++ server.go
@@ -3,6 +3,9 @@ package main
 import (
   "github.com/go-martini/martini"
   "github.com/codegangsta/martini-contrib/render"
+  "math/rand"
+  "time"
+  "./lib"
 )
 
 func main() {
@@ -13,12 +16,13 @@ func main() {
     Extensions: []string{".html"},
   }))
 
-  m.Get("/", func() string {
-    return "Welcome to seedot"
+  m.Get("/", func(r render.Render) {
+    random_string := randomString(10, 30)
+    r.HTML(200, "main", "/d/" + random_string)
   })
 
-  m.Get("/d/:query", func(params martini.Params) string {
-    return params["query"]
+  m.Get("/d/:query", func(params martini.Params, r render.Render) {
+    r.HTML(200, "d", params["query"])
   })
 
   m.Run()
